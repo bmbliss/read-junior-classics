e2b14fab26587deec0f5af36f801a8d77bfe7eae
Brendan Bliss <brendan.bliss@flybreeze.com>
2024-09-18 23:01:25 -0600

    some layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
   before_action :require_current_user
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def show
   end
