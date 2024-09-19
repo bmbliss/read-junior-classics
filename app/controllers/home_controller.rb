576c9bc447c39d430647abc7848685ae8638f670
Brendan Bliss <brendan.bliss@flybreeze.com>
2024-09-18 23:50:22 -0600

    better home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,11 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      @children = current_user.children.includes(program_enrollments: :program)
+    end
+    
     @featured_programs = Program.order(created_at: :desc).limit(3)
     @recent_literary_works = LiteraryWork.order(created_at: :desc).limit(5)
-
-    # Test flash messages
-    # flash.now[:notice] = "This is a notice message"
-    # flash.now[:success] = "This is a success message"
-    # flash.now[:error] = "This is an error message"
-    # flash.now[:alert] = "This is an alert message"
+    @collections = Collection.order(created_at: :desc).limit(3)
   end
 end
