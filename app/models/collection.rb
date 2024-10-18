5321e8b557140f854b8e1e7c307393f322b77dda
Brendan Bliss <brendan.bliss@flybreeze.com>
2024-10-18 12:30:52 -0600

    add estimates

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,10 @@
 class Collection < ApplicationRecord
   has_and_belongs_to_many :literary_works
+
+  def total_reading_time
+    total_min = literary_works.sum(:estimated_reading_time)
+    total_hours = (total_min / 60).floor
+    total_minutes = total_min % 60
+    "#{total_hours} hr #{total_minutes} min"
+  end
 end
